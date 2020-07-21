11c76ba2325d5f4f6e0c0ee0cf5dfccabee6f8d2
wildwing <ggipk1010@gmail.com>
2020-07-21 21:34:39 +0900

    내일은 서비스부터 만들기

---
+++ src/main/java/com/example/jjudrestaurant/JjudrestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.example.jjudrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjudrestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjudrestaurantApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/jjudrestaurant/VO/Memberjjud.java
@@ -0,0 +1,11 @@
+package com.example.jjudrestaurant.VO;
+
+import lombok.Data;
+
+@Data
+public class Memberjjud {
+	private int id; 
+	private String email;
+	private String password; 
+	private String userRole; 
+}
